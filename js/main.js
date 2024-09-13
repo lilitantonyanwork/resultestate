ca82032a7e805d61d7d5e43ca8ac599769074858
lilitantonyan <lilit@helix.am>
2024-09-13 14:28:01 +0400

    add project

--- js/main.js
+++ js/main.js
@@ -84,17 +84,118 @@ $(function (){
         $('#video').get(0).play()
     })
 
+    // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+
+    function init() {
+        // Создание карты.
+        // https://tech.yandex.ru/maps/doc/jsapi/2.1/dg/concepts/map-docpage/
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчнию: «широта, долгота».
+            center:[52.09223207204043,23.688205499999917],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 18,
+            nightModeEnabled: true,
+            iconImageHref: 'img/marker.png',
+
+            theme: 'dark',
+            // Элементы управления
+            // https://tech.yandex.ru/maps/doc/jsapi/2.1/dg/concepts/controls/standard-docpage/
+            controls: [
+
+                'zoomControl', // Ползунок масштаба
+                'rulerControl', // Линейка
+                //'routeButtonControl', // Панель маршрутизации
+                //'trafficControl', // Пробки
+                //'typeSelector', // Переключатель слоев карты
+                'fullscreenControl', // Полноэкранный режим
+
+                // Поисковая строка
+                /*new ymaps.control.SearchControl({
+                    options: {
+                        // вид - поисковая строка
+                        size: 'large',
+                        // Включим возможность искать не только топонимы, но и организации.
+                        provider: 'yandex#search'
+                    }
+                })*/
+
+            ]
+        });
+
+
+        // Добавление метки
+        // https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/Placemark-docpage/
+        var myPlacemark = new ymaps.Placemark([52.09223207204043,23.688205499999917], {
+            // Хинт показывается при наведении мышкой на иконку метки.
+            hintContent: 'Всплывающая подсказка на Yandex карте <a href="https://problesk.com" target="_blank" >Ссылка </a>',
+            // Балун откроется при клике по метке.
+            balloonContent: 'Содержимое балуна, появляющееся при клике на него <a href="https://problesk.com" target="_blank" >Ссылка </a>'
+        });
+
+        // После того как метка была создана, добавляем её на карту.
+        myMap.geoObjects.add(myPlacemark);
+        setMapLayer();
+
+    }
+    ymaps.ready(init);
+
+    function setMapLayer(){
+        const DARK_MAP = 'custom#dark';
+        ymaps.layer.storage.add(DARK_MAP, function DarkLayer() {
+            // Ссылка на темные тайлы Яндекс.Карт
+            // От стандартной отличается наличием &theme=dark
+            return new ymaps.Layer(
+                'https://core-renderer-tiles.maps.yandex.net/tiles?l=map&theme=dark&%c&%l&scale={{ scale }}',
+            );
+        });
+
+        /* Связываем слой с типом карты */
+        ymaps.mapType.storage.add(DARK_MAP, new ymaps.MapType('Dark Map', [DARK_MAP]));
+    }
+    function init() {
+
+        var myMap = new ymaps.Map("map", {
+            center: [52.09223207204043,23.688205499999917],
+            zoom: 14,
+            nightModeEnabled: true,
+            theme: 'dark',
+            iconImageHref: 'img/marker.png',
+        });
+        myMap.controls.remove('searchControl'); // удаляем поиск
+        myMap.controls.remove('trafficControl'); // удаляем контроль трафика
+        myMap.controls.remove('typeSelector'); // удаляем тип
+        myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+        myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
+        myMap.controls.remove('rulerControl'); // удаляем контрол правил
+        myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+
+        // Все виды меток
+        // https://tech.yandex.ru/maps/doc/jsapi/2.1/ref/reference/option.presetStorage-docpage/
+
+        // Метка без содержимого с точкой в центре
+        var placemark1 = new ymaps.Placemark([52.09223207204043,23.688205499999917], {
+
+        }, {
+            'preset': 'islands#nightDotIcon',
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/marker.png',
+            // Размеры метки.
+            iconImageSize: [44, 53],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-11,-53]
+        });
+
+        myMap.geoObjects.add(placemark1);
+
+
+    }
 
 })
-let map;
-
-async function initMap() {
-    const { Map } = await google.maps.importLibrary("maps");
-
-    map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-    });
-}
-
-initMap();
